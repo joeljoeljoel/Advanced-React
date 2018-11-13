f12e6d3ad9b9d04d9b768f65e3711288d62c1e83
Joel Jacob <joel@viastudio.com>
2018-11-13 16:31:39 -0500

    Modules 1 - 3

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,13 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env'});
+const createSever = require('./createServer');
+const db = require('./db');
+
+const server = createSever();
+server.start({
+    cors: {
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    },
+}, deets => {
+    console.log(`Server is now running on port http://localhost:${deets.port}`);
+});
